c67fbda095d6c542445a16455800ec2a8306f1c6
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2026-05-15 15:47:14 +0500

    cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors')
 
 const campaignRoutes = require('./routes/campaignRoutes');
 const leadRoutes = require('./routes/leadRoutes')
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors())
 app.use("/files", express.static(path.join(__dirname, "files")));
 
 app.use('/api', campaignRoutes);
